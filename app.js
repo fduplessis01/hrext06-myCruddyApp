c1a5231ae8e4b58e23bc480edf88f2aca83cca69
fawnieduplessis <fduplessis01@gmail.com>
2018-12-17 14:40:48 -0600

    Take in multiple values by using multiple keys. Create new keynames each time

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
   console.log('jQuery loaded');
 
+  //var myItemInStorage = {};
+
   // write to local storage from input when button save clicked
   $('.btn-submit').on('click', function(){
-    localStorage.setItem('inputFieldValue', $('.text-entry').val());
-    var myItemInStorage = localStorage.getItem('inputFieldValue');
+    //localStorage.setItem('inputFieldValue', $('.text-entry').val());
+    //localStorage.setItem("lastname", "Smith");
+    localStorage.setItem($('.text-discription').val(), $('.text-entry').val());
+    //var myItemInStorage = localStorage.getItem('inputFieldValue');
+    var key = $('.text-discription').val();
+    console.log('key', key)
+    var myItemInStorage = localStorage.getItem(key);
     console.log('myItemInStorage', myItemInStorage);
 
     // display the value here
@@ -14,7 +21,8 @@ $(document).ready(function(){
 
   // delete from local storage when delete button clicked
   $('.btn-delete').on('click', function(){
-    localStorage.removeItem('inputFieldValue');
+    //localStorage.removeItem('inputFieldValue');
+    localStorage.removeItem($('.text-discription').val());
   });
   // delete all from local storage when delete all button clicked
   $('.btn-delete-all').on('click', function () {
@@ -22,3 +30,10 @@ $(document).ready(function(){
   });
 
 });
+
+
+
+// // Store
+// localStorage.setItem("lastname", "Smith");
+// // Retrieve
+// document.getElementById("result").innerHTML = localStorage.getItem("lastname");
